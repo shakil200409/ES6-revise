8b8aecf742a87a079e83f0b9be78ee247a661669
Shakil Ahmed <shakil200409@gmail.com>
2024-12-03 12:23:59 +0600

    Practiced JSON stringify and parse, learned to get the keys and values of an object, revised how to add an object to copied array

---
+++ fetching.js
@@ -0,0 +1,36 @@
+const person = {
+  name: "Sakib Khan",
+  age: 36,
+  movies: ["King Khan", "Password", "Shikar"],
+};
+
+// making an object to JSON string
+const personString = JSON.stringify(person);
+// console.log(person);
+// console.log(personString);
+
+// making a JSON string to JavaScript Object
+const personObj = JSON.parse(personString);
+// console.log(personObj);
+
+// getting value and keys from an object
+const keys = Object.keys(person);
+// console.log(keys);
+
+const values = Object.values(person);
+// console.log(values);
+
+// Add or Remove from an Array
+const products = [
+  { name: "laptop", price: 20000, brand: "HP", color: "Silver" },
+  { name: "Phone", price: 15000, brand: "Samsung", color: "Black" },
+  { name: "Watch", price: 5000, brand: "Casio", color: "Brown" },
+  { name: "Buds", price: 1500, brand: "Lenovo", color: "White" },
+];
+
+const newProduct = {name: 'webCam', price: 1000, brand: 'Canon', color: 'white'};
+
+// Copying products array and adding the new object to the array
+const newProducts = [...products, newProduct];
+// console.log(products);
+// console.log(newProducts);
